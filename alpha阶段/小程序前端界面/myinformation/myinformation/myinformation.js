bbc45aeaf577ce98d8058765f3c3bc2d7f8cd765
cyj-12138 <70762697+cyj-12138@users.noreply.github.com>
2020-11-17 18:22:15 +0800

    更新界面

---
+++ alpha阶段/小程序前端界面/myinformation/myinformation/myinformation.js
@@ -0,0 +1,19 @@
+Page({
+
+  data: {
+    name:'羊抄阅',
+    personal: '认识你们，很开心~',
+    TEL:'185-6666-8888',
+    Email:'123456@qq.com'
+  },
+
+  Changeinformation(event) {
+    console.log(event.detail);
+  },
+
+  
+  update(event){
+
+  }
+  
+})
