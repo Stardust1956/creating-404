ebc1cb2beb948090d2db0346b4fdedc751cdb6a8
Hang <1145900873@qq.com>
2020-12-06 18:31:31 +0800

    自动化测试autojs

---
+++ 测试代码/自动加书.js
@@ -0,0 +1,17 @@
+var n = 1000
+var i=n
+while(i--)
+{
+  click(760,1850);
+  sleep(500)
+  back();
+  sleep(500)
+  click(960,1850);
+}
+i=n
+click(540,2210)
+sleep(500)
+while(i--)
+{
+  click(535,1685);
+}
